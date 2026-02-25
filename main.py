12a370a1ae78661563c3602f19522a399d1475ca
whatwant <whatwant@gmail.com>
2026-02-25 23:13:43 +0900

    feat: add basic arithmetic functions

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ load_dotenv()
 debug_mode = os.getenv("DEBUG")
 
 
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
 def main():
     print(f"Debug Mode: {debug_mode}")
+    print(f"1 + 2 = {add(1, 2)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
 
 
 if __name__ == "__main__":
